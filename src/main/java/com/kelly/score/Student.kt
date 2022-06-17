5c999bf8e15fdc2b5b9e6f5820bb6f61c8cd91ab
kelllly322 <kellylan322@gmail.com>
2022-06-17 15:21:48 +0800

    Student.kt

--- src/main/java/com/kelly/score/Student.kt
+++ src/main/java/com/kelly/score/Student.kt
@@ -1,11 +1,16 @@
 package com.kelly.score
 
-class Student {
-    var name = ""
-    var english = 0
-    var math = 0
+class Student (var name:String, var english:Int, var math:Int){
+    init {
+       // println("Testing")
+    }
+    fun  print(){
+        println("$name\t$english\t$math\t${getAverage() }")
+    }
+    fun  getAverage() =(english+math)/2
 
 }
 fun main(){
     val stu = Student("Jack", 55, 87)
+    stu.print()
 }
